bc592666bf12b19b3ccbbd822a17d8c074ddcc64
Brendan Shephard <bshephar@redhat.com>
2024-12-01 10:58:18 +1000

    Add neogit
    Signed-off-by: Brendan Shephard <bshephar@redhat.com>

--- init.lua
+++ init.lua
@@ -1000,6 +1000,7 @@ require('lazy').setup({
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.trouble',
   require 'custom.theme',
+  require 'custom.plugins.neogit',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.

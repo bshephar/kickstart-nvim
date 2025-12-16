9f836606c8463d9de1566408771fe74b1709db95
Brendan Shephard <bshephard@nvidia.com>
2025-12-16 10:57:55 +1000

    Configure snacks
    Signed-off-by: Brendan Shephard <bshephard@nvidia.com>

--- lua/custom/snacks.lua
+++ lua/custom/snacks.lua
@@ -9,6 +9,7 @@ return {
       dashboard = { enabled = true },
       indent = { enabled = true },
       input = { enabled = true },
+      picker = { enabled = true },
       notifier = {
         enabled = true,
         timeout = 3000,
@@ -24,8 +25,51 @@ return {
       },
     },
     keys = {
-      {
-        '<leader>z',
+      -- Top Pickers & Explorer
+      { '<leader><space>', function() Snacks.picker.buffers() end, desc = 'Buffers' },
+      { '<leader>/', function() Snacks.picker.grep() end, desc = 'Grep' },
+      { '<leader>:', function() Snacks.picker.command_history() end, desc = 'Command History' },
+      { '<leader>n', function() Snacks.picker.notifications() end, desc = 'Notification History' },
+      { '<leader>e', function() Snacks.explorer() end, desc = 'File Explorer' },
+      -- Find
+      { '<leader>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
+      { '<leader>fc', function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = 'Find Config File' },
+      { '<leader>ff', function() Snacks.picker.files() end, desc = 'Find Files' },
+      { '<leader>fg', function() Snacks.picker.git_files() end, desc = 'Find Git Files' },
+      { '<leader>fr', function() Snacks.picker.recent() end, desc = 'Recent' },
+      -- Git
+      { '<leader>gl', function() Snacks.picker.git_log() end, desc = 'Git Log' },
+      { '<leader>gs', function() Snacks.picker.git_status() end, desc = 'Git Status' },
+      -- Grep
+      { '<leader>sb', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
+      { '<leader>sB', function() Snacks.picker.grep_buffers() end, desc = 'Grep Open Buffers' },
+      { '<leader>sg', function() Snacks.picker.grep() end, desc = 'Grep' },
+      { '<leader>sw', function() Snacks.picker.grep_word() end, desc = 'Visual selection or word', mode = { "n", "x" } },
+      -- Search
+      { '<leader>s"', function() Snacks.picker.registers() end, desc = 'Registers' },
+      { '<leader>sa', function() Snacks.picker.autocmds() end, desc = 'Autocmds' },
+      { '<leader>sc', function() Snacks.picker.command_history() end, desc = 'Command History' },
+      { '<leader>sC', function() Snacks.picker.commands() end, desc = 'Commands' },
+      { '<leader>sd', function() Snacks.picker.diagnostics() end, desc = 'Diagnostics' },
+      { '<leader>sh', function() Snacks.picker.help() end, desc = 'Help Pages' },
+      { '<leader>sH', function() Snacks.picker.highlights() end, desc = 'Highlights' },
+      { '<leader>sk', function() Snacks.picker.keymaps() end, desc = 'Keymaps' },
+      { '<leader>sl', function() Snacks.picker.loclist() end, desc = 'Location List' },
+      { '<leader>sm', function() Snacks.picker.marks() end, desc = 'Marks' },
+      { '<leader>sM', function() Snacks.picker.man() end, desc = 'Man Pages' },
+      { '<leader>sp', function() Snacks.picker.lazy() end, desc = 'Search for Plugin Spec' },
+      { '<leader>sq', function() Snacks.picker.qflist() end, desc = 'Quickfix List' },
+      { '<leader>sR', function() Snacks.picker.resume() end, desc = 'Resume' },
+      { '<leader>su', function() Snacks.picker.undo() end, desc = 'Undo History' },
+      -- LSP
+      { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
+      { 'gr', function() Snacks.picker.lsp_references() end, nowait = true, desc = 'References' },
+      { 'gI', function() Snacks.picker.lsp_implementations() end, desc = 'Goto Implementation' },
+      { 'gy', function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto T[y]pe Definition' },
+      { '<leader>ss', function() Snacks.picker.lsp_symbols() end, desc = 'LSP Symbols' },
+      { '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols' },
+      -- Other
+      { '<leader>z',
         function()
           Snacks.zen()
         end,
